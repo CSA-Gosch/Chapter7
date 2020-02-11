c8103af00a58cda30dc859bcc36bd3867ab05403
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2020-02-11 12:50:13 -0600

    bruh

--- src/Exercise/PigLatin.java
+++ src/Exercise/PigLatin.java
@@ -8,14 +8,22 @@ public class PigLatin {
         String word;
         char vowel;
         String ay = "ay";
+        String remaining;
+
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a word to be converted >>>");
+
         word = input.nextLine();
         vowel = word.charAt(0);
 
-        if(vowel == 'a' || vowel == 'e'||vowel == 'i' ||vowel = 'o' ||vowel = 'u'){
+        if(vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u'){
             System.out.print(word + ay);
         }
+        else{
+            remaining = word.substring(1, word.length());
+            System.out.print(remaining + vowel + ay);
+        }
+
 
     }
 }
